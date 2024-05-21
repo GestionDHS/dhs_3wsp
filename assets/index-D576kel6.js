390d3ce1d1b10080f7385385fd32487860fd4f97
María Pía Achigar <achigarpia@gmail.com>
2024-05-21 18:30:07 -0300

    Updates

--- assets/index-D576kel6.js
+++ assets/index-D576kel6.js
@@ -1,4 +1,4 @@
-import"./style-CFd07vcV.js";/* empty css                  */const s="/dhs_3wsp/assets/logodhs-D3ek8met.png";document.querySelector("#app").innerHTML=`
+import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css                  */const s="/dhs_3wsp/assets/logodhs-D3ek8met.png";document.querySelector("#app").innerHTML=`
   <div>
     <a href="https://www.digitalhouse.com/ar/productos/escuelas">
       <h1>Actividades Tres Workspaces</h1>
